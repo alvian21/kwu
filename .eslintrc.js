be5482b4acd8ea22923735f0b86dff42242f188a
Alvian Ardhiansyah <alvian.ardhiansyah@gmail.com>
2020-12-05 19:58:29 +0700

    revisi

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,27 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        //Add prettier formatting rules here
+      },
+    ],
+    //Add ESLint linting rules here
+    "no-console": "off",
+    "no-unused-vars": "warn",
+  },
 };
