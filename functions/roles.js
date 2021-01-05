bcd002b2e84b71250c05dd9e5fbc46e0b2e08ad0
Alvian Ardhiansyah <alvian.ardhiansyah@gmail.com>
2021-01-05 21:08:17 +0700

    revisi

--- functions/roles.js
+++ functions/roles.js
@@ -2,9 +2,9 @@ const AccessControl = require("accesscontrol");
 const ac = new AccessControl();
 
 exports.roles = (function () {
-  ac.grant("member").readOwn("profile").readAny("post");
+  ac.grant("member").readOwn("profile").readAny("post").readAny("designer");
 
-  ac.grant("designer").readAny("post").createOwn("post");
+  ac.grant("designer").readAny("post").createOwn("post").readAny("member");
 
   ac.grant("admin").readAny("member").readAny("designer");
   return ac;
